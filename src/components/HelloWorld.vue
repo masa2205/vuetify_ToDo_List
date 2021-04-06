7f04fc64a90cf929f3f40b711069419ee4969489
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-07 02:44:22 +0900

    コード整理

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,22 +5,12 @@
         <v-toolbar-title>ToDo List</v-toolbar-title>
       </v-app-bar>
       <v-card class="mx-auto" outlined max-width="1200">
-       <v-card-title> 
-       <v-text-field label="入力欄" placeholder="ToDoの入力" outlined></v-text-field>
-        <v-btn color="primary" dark class="mb-2" @click="add()">追加</v-btn>
-      </v-card-title>  
-       <v-checkbox
-         v-model="showMessage"
-         :label="`showMessage is ${showMessage}`"
-         color="success">
-       </v-checkbox>
-       <v-date-picker
-        v-model="picker"
-        reactive
-        locale="jp-ja"
-        :day-format="date => new Date(date).getDate()"
-       ></v-date-picker>
-       <v-footer color="info" dark app>Vuetify</v-footer>
+        <v-card-title> 
+          <v-text-field label="入力欄" placeholder="ToDoの入力" outlined></v-text-field>
+            <v-btn color="primary" dark class="mb-2">追加</v-btn>
+        </v-card-title> 
+       <v-date-picker v-model="picker" reactive locale="jp-ja" :day-format="date => new Date(date).getDate()"></v-date-picker>
+       <v-footer color="info" dark app>ToDo List</v-footer>
       </v-card> 
     </v-container>
   </v-app>
